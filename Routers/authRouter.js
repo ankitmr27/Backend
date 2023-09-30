95e8f74aa8d1857d86a1d08bc17cc366a6b13e6e
ankitmr27 <am5099951@gmail.com>
2023-09-30 06:26:51 +0530

    MVC

--- Routers/authRouter.js
+++ Routers/authRouter.js
@@ -1,59 +1,13 @@
 const express = require("express");
 require("dotenv").config();
-const jwt = require("jsonwebtoken");
-const userModel = require("../models/userModel.js");
-const JWT_KEY = process.env.JWT_KEY;
+const {
+  getSignup,
+  postSignup,
+  loginUser,
+} = require("../controller/authController");
+
 //auth routes
 const authRouter = express.Router();
-
-function getSignup(req, res) {
-  res.sendFile("/public/index.html", { root: __dirname });
-}
-
-async function postSignup(req, res) {
-  try {
-    let dataObject = req.body;
-    //console.log("backend", dataObject);
-    let user = await userModel.create(dataObject);
-    res.json({
-      message: "user Signed up",
-      data: dataObject,
-    });
-    console.log("created user:", user);
-  } catch (e) {
-    console.log(e);
-  }
-}
-
-async function loginUser(req, res) {
-  if (req.body.email) {
-    try {
-      let user = await userModel.findOne({ email: req.body.email });
-      if (user) {
-        //bcrypt has be to taken care of
-        if (user.password === req.body.password) {
-          let uid = user["_id"]; // unique id
-          // this function will include header itself default
-          let JWTtoken = jwt.sign({ payload: uid }, JWT_KEY); // by default- HMAC-SHA256 but to specify{algorithm:'RS256'}
-          //setting the cookie as true for logged in user
-          res.cookie("isLoggedIn", JWTtoken, { httpOnly: true }); // sending single cookie
-          return res.json({
-            message: "User Found",
-            userDetails: req.body,
-          });
-        } else {
-          return res.json({ message: "Invalid Credential" });
-        }
-      } else {
-        return res.json({ message: "User not found" });
-      }
-    } catch (e) {
-      console.log(e);
-    }
-  } else {
-    res.json({ message: "Empty email found" });
-  }
-}
 
 authRouter.route("/signup").get(getSignup).post(postSignup);
 authRouter.route("/login").post(loginUser);
